166a635efbfc5351abc626f3d7c8497a7508e865
Eduard Cristian Tasca <eduardcristiantasca@gmail.com>
2022-07-06 00:16:07 +0300

    More icons

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,6 @@
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function Footer() {
   return (
     <footer className="footer">
@@ -5,9 +8,9 @@ export default function Footer() {
         <span>© devedco 2022</span>
         <br />
         <span>Social:&nbsp;</span>
-        <a href="https://www.linkedin.com/in/eduard-cristian-ta%C5%9Fc%C4%83-82330a211/" target="_blank" rel="noreferrer">LinkedIn</a>
+        <a href="https://www.linkedin.com/in/eduard-cristian-ta%C5%9Fc%C4%83-82330a211/" target="_blank" rel="noreferrer">LinkedIn <FontAwesomeIcon icon={faLinkedin} /> </a>
         &nbsp;
-        <a href="#">Github</a>
+        <a href="https://github.com/tasca-eduard" target="_blank" rel="noreferrer">Github <FontAwesomeIcon icon={faGithub} /> </a>
         <br />
         <span>Contact: devedco@outlook.com</span>
       </div>
