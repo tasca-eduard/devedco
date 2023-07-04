c2af93cb8fbad7292807a7e8997ddbc699de0715
tasca-eduard <eduardcristiantasca@gmail.com>
2023-07-04 15:31:02 +0300

    Updated resume

--- src/components/Portofolio/Portofolio.tsx
+++ src/components/Portofolio/Portofolio.tsx
@@ -2,7 +2,7 @@ export default function Portofolio() {
   return (
     <div className="container">
       <h2 id="portofolio">Projects</h2>
-      <p>Comming soon.</p>
+      <p>Coming soon.</p>
       <small>I have no idea what I'm doing. Who knows, maybe I'll burn this site if I get bored.</small>
     </div>
   )
